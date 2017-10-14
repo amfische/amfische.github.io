34fcaa79fb5299776e5f78a0b896a646fab5fa62
amfische <amfischer21@gmail.com>
2017-10-13 17:55:27 -0700

    hotfix - styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,11 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-	$('.navbar-side').css('width', '275px');
-	$('.main-content').css('margin-left', '275px');
+	$('.navbar-side').css('width', '100vw');
+	$('.main-content').css('margin-left', '100vw');
 
 }
+
+/*275px*/
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
