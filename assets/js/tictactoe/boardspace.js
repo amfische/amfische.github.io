d3ddf467763e5779afe65c0996cc8d347f156e51
amfische <amfischer21@gmail.com>
2017-12-24 11:37:18 -0700

    small edits, changed vue cdn

--- assets/js/tictactoe/boardspace.js
+++ assets/js/tictactoe/boardspace.js
@@ -1,13 +1,5 @@
 "use strict"
 
-//each space is an object
-// function BoardSpace(x,y) {
-// 	this.occupied = false;
-// 	this.value = "";
-// 	this.XCoordinate = x;
-// 	this.YCoordinate = y;
-// }
-
 function BoardSpace(x,y) {
 	this.occupied = false;
 	this.value = "";
