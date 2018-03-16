e01a21f4b35ff78c0a7c4f65835a97da67619308
amfische <amfischer21@gmail.com>
2018-03-16 15:31:59 -0700

    trying to get mobile nav to slide down

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,7 +6,8 @@ new Vue({
 	methods: {
 		toggleNavbar() {
 			this.isMobile = !this.isMobile
-			this.$refs.navbrand.style.borderBottom = this.isMobile ? '1px solid black' : 'none'
+			// this.$refs.navbrand.style.borderBottom = this.isMobile ? '1px solid black' : 'none'
+			this.$refs.navmenu.style.marginTop = this.isMobile ? '0px' : '-300px'
 		}
 	}
 })
